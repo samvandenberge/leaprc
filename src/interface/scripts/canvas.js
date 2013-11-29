a5b68f8e5ca5eb3fcbcc617c8830598d0d6d965f
Pieter Van Molle <pietervanmolle@Pieters-MacBook-Air.local>
2013-11-29 13:18:46 +0100

    Remove listeners and unused functions

--- src/interface/scripts/canvas.js
+++ src/interface/scripts/canvas.js
@@ -8,14 +8,6 @@ function Slider (x, y, w, h, fill) {
 	this.throttle = 0;
 };
 
-Slider.prototype.increasePercentage = function () {
-	this.throttle = this.throttle + .02 > 1 ? 1 : this.throttle + .02;
-}
-
-Slider.prototype.decreasePercentage = function() {
-	this.throttle = this.throttle - .02 < 0 ? 0 : this.throttle - .02;
-}
-
 Slider.prototype.draw = function(ctx) {
 	ctx.fillStyle = '#000';
 	ctx.fillRect(this.x - 2, this.y - 2, this.w + 4, this.h + 4);
@@ -25,11 +17,6 @@ Slider.prototype.draw = function(ctx) {
 
 	ctx.fillStyle = this.fill;
 	ctx.fillRect(this.x, this.y + this.h, this.w, -1 * this.h * this.throttle);
-};
-
-Slider.prototype.contains = function(mx, my) {
-	return (this.x <= mx) && (this.x + this.w >= mx) &&
-	       (this.y <= my) && (this.y + this.h >= my);
 };
 
 function Joystick(x, y, radius, innerRadius) {
@@ -65,10 +52,6 @@ Joystick.prototype.draw = function(ctx) {
 	ctx.fill();
 }
 
-Joystick.prototype.contains = function(mx, my) {
-	return (mx - this.x) * (mx - this.x) + (my - this.y) * (my - this.y) < this.radius * this.radius;
-}
-
 function CanvasState (canvas) {
 	this.canvas = canvas;
 	this.width  = canvas.width;
@@ -90,92 +73,9 @@ function CanvasState (canvas) {
 	this.joystick  = new Joystick();
 
 	this.valid     = false;
-	this.dragging  = false;
-	this.selection = null;
-	this.dragoffx  = 0;
-	this.dragonffy = 0;
-
-	var state = this;
-
-	canvas.addEventListener('selectstart', function(e) { e.preventDefault(); return false; }, false);
-
-	canvas.addEventListener('mousedown', function (e) {
-		var mouse    = state.getMouse(e),
-		    mx       = mouse.x,
-		    my       = mouse.y
-		    slider   = state.slider,
-		    joystick = state.joystick;
-
-		if (slider.contains(mx, my)) {
-			state.dragoffx  = mx - slider.x;
-			state.dragonffy = my - slider.y;
-			state.dragging  = true;
-			state.selection = slider;
-			state.valid     = false;
-			return;
-		}
-
-		if (joystick.contains(mx, my)) {
-			state.dragoffx  = mx - slider.x;
-			state.dragonffy = my - slider.y;
-			state.dragging  = true;
-			state.selection = joystick;
-			state.valid     = false;
-			return;
-		}
-
-		if (state.selection) {
-			state.selection = null;
-			state.valid     = false;
-		}
-	}, true);
-
-	canvas.addEventListener('mousemove', function(e) {
-		var mouse = state.getMouse(e);
-
-		if (!state.dragging) {
-			return;
-		}
-
-		if (state.selection == state.slider && state.selection.contains(mouse.x, mouse.y)) {
-			var p = (state.getMouse(e).y - state.selection.y) / state.selection.h;
-
-			state.selection.throttle = 1 - p;
-			state.valid                = false;
-		}
-
-		if (state.selection == state.joystick && state.selection.contains(mouse.x, mouse.y)) {
-			var pX = (1 + ((state.getMouse(e).x - state.selection.x) / state.selection.radius)) / 2;
-			var pY = (1 - ((state.getMouse(e).y - state.selection.y) / state.selection.radius)) / 2;
-
-			state.selection.yaw   = pX;
-			state.selection.pitch = pY;
-			state.valid           = false;
-		}
-	}, true);
-
-	canvas.addEventListener('mouseup', function(e) {
-		state.dragging = false;
-	}, true);
-
-	window.addEventListener('keydown', function(e) {
-		var event = window.event || e;
-		
-		if (event.keyCode === 38) {
-			state.slider.increasePercentage();
-			state.valid = false;
-			return;
-		}
-
-		if (event.keyCode === 40) {
-			state.slider.decreasePercentage();
-			state.valid = false;
-			return;
-		}
-	});
 
 	this.interval = 30;
-	setInterval(function() { state.draw(); }, state.interval);
+	setInterval(function() { this.draw(); }, this.interval);
 };
 
 CanvasState.prototype.setSlider = function(slider) {
@@ -205,25 +105,6 @@ CanvasState.prototype.draw = function() {
 	this.joystick.draw(this.ctx);
 
 	this.valid = true;
-};
-
-CanvasState.prototype.getMouse = function(e) {
-	var element = this.canvas, offsetX = 0, offsetY = 0, mx, my;
-
-	if (element.offsetParent !== undefined) {
-		do {
-			offsetX += element.offsetLeft;
-			offsetY += element.offsetTop;
-		} while (element = element.offsetParent);
-	}
-
-	offsetX += this.stylePaddingLeft + this.styleBorderLeft + this.htmlLeft;
-	offsetY += this.stylePaddingTop + this.styleBorderTop + this.htmlTop;
-
-	mx = e.pageX - offsetX;
-	my = e.pageY - offsetY;
-
-	return {x: mx, y: my};
 };
 
 CanvasState.prototype.setThrottle = function(throttle) {
